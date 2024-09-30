d3d0ed0403f2844c58f8c550bfb8ce4383542787
Max Hastings <max.jacob.hastings@gmail.com>
2024-09-30 13:54:09 -0500

    make sort use cases injectable

--- app/src/main/java/com/adyen/android/assignment/usecases/SortPicturesByTitleUseCase.kt
+++ app/src/main/java/com/adyen/android/assignment/usecases/SortPicturesByTitleUseCase.kt
@@ -1,8 +1,9 @@
 package com.adyen.android.assignment.usecases
 
 import com.adyen.android.assignment.api.model.AstronomyPicture
+import javax.inject.Inject
 
-class SortPicturesByTitleUseCase {
+class SortPicturesByTitleUseCase @Inject constructor() {
     operator fun invoke(pictures: List<AstronomyPicture>, ascending: Boolean): List<AstronomyPicture> {
         return if (ascending) {
             pictures.sortedBy { it.title }
