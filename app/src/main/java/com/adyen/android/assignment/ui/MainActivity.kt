83cac74cf1c45f24932a39e2e5e5c6cdb35c0531
Max Hastings <max.jacob.hastings@gmail.com>
2024-09-30 12:21:07 -0500

    added hilt entry points

--- app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.adyen.android.assignment.ui
 
 import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity()
