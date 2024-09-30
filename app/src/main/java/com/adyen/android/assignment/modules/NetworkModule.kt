926466c9beaaa1d4a0a488b716d9ea5578272d80
Max Hastings <max.jacob.hastings@gmail.com>
2024-09-30 13:52:03 -0500

    add network module

---
+++ app/src/main/java/com/adyen/android/assignment/modules/NetworkModule.kt
@@ -0,0 +1,43 @@
+package com.adyen.android.assignment.modules
+
+import com.adyen.android.assignment.BuildConfig
+import com.adyen.android.assignment.api.PlanetaryService
+import com.adyen.android.assignment.api.model.DayAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(DayAdapter())
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.NASA_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePlanetaryService(retrofit: Retrofit): PlanetaryService {
+        return retrofit.create(PlanetaryService::class.java)
+    }
+}
